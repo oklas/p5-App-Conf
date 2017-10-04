553c3aad220aad89e4c1d1862b15bc48a30e381d
Serguei Okladnikov <oklaspec@gmail.com>
2017-10-04 22:59:36 +0400

    initial commit (minil new App::conf)

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Conf - It's new $module
+
+# SYNOPSIS
+
+    use Conf;
+
+# DESCRIPTION
+
+Conf is ...
+
+# LICENSE
+
+Copyright (C) Serguei Okladnikov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Serguei Okladnikov <oklaspec@gmail.com>
---
+++ lib/Conf.pm
@@ -0,0 +1,39 @@
+package Conf;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Conf - It's new $module
+
+=head1 SYNOPSIS
+
+    use Conf;
+
+=head1 DESCRIPTION
+
+Conf is ...
+
+=head1 LICENSE
+
+Copyright (C) Serguei Okladnikov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Serguei Okladnikov E<lt>oklaspec@gmail.comE<gt>
+
+=cut
+
